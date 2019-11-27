dd1307bc93142da69e40902d4e7766043d0ba8d9
TommyKeating <tommykeating@wustl.edu>
2019-11-27 15:43:57 -0600

    screen displays

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>ToDo App</h1>
+      <div className="Page">
+        <h1>Tommy Keating's ToDo App</h1>
+        <div class = "centerpage">
+          <div class = "todoform">
+            <div class = "addform">
+            <form onsubmit="return false">
+              <h2>Create ToDo Item Form</h2>
+              <input type = "text" id = "name"></input><br /><br />
+              <button class = "createB" id = "addButton">Create</button>
+            </form>
+            </div>
+          </div>
+          <div class = "todolist">
+            <div class = "ltitle"><h2>ToDo List</h2></div>
+            <div class = "listbody"><ul id = "ListOfToDos"></ul></div>
+          </div>
+        </div>
       </div>
     );
   }
